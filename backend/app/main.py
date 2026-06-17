712299cd57611a9d833f0c1062fb79f2bf0b2998
Lakshmi Prasad <Lakshmi.Prasad@PlanetI.local>
2026-06-17 17:27:38 +0530

    updated backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def meow():
     return {
   "message": "AI Interior Copilot Backend Running"
 }
+@app.get("/health")
+def healthcheck():
+    return{
+        "Status": "healthy",
+        "service":"AI Interior Copilot Backend",
+        "version": "1.0"
+        }
+class dataentry(BaseModel):
+    room_type:str
+    style:str
+    budget:int
+@app.post("/recommend")
+def recommend(data : dataentry):
+    if data.room_type == "Bedroom" and data.style == "modern" and data.budget == 100000:
+        return{
+        "message":"data recieved",
+        "recommendation":"go fuck yourself"
+        }
+    
